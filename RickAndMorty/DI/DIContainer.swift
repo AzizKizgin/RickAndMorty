a48be87bc9aee7f068b2f102f3d192751c55b6f5
Aziz Kızgın <azizkzgn@gmail.com>
2025-05-03 22:12:51 +0300

    di container added

---
+++ RickAndMorty/DI/DIContainer.swift
@@ -0,0 +1,22 @@
+//
+//  DIContainer.swift
+//  RickAndMorty
+//
+//  Created by Aziz Kızgın on 3.05.2025.
+//
+import Foundation
+import Swinject
+class DIContainer {
+    static let shared = DIContainer()
+    let container = Container()
+    
+    private init() {
+        registerDependencies()
+    }
+
+    private func registerDependencies() {
+        container.register(CharacterService.self) { _ in
+            CharacterService()
+        }
+    }
+}
